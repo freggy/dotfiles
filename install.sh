cb483ea9ac86f58b2565d61956dd91de12718221
Yannic Bastian Rieger <riegeryannic@gmail.com>
2022-11-10 18:58:05 +0100

    start work on install script

---
+++ install.sh
@@ -0,0 +1,45 @@
+#!/bin/bash -e
+
+function copy_files {
+	# find all paths to files in $dir	
+	IFS=$'\n' files=($(find $dir -mindepth 1 -type f))
+
+	for file in "${files[@]}"
+	do
+		dirs=$(dirname $file)
+
+		# copy files to home directory
+		if [ "$1" = true ]; 
+		then
+			home=${HOME:1} # home path without the leading /
+			dirs="${dirs/__homedir/$home}"
+		fi
+		echo "creating directories if not exist: /$dirs"
+		echo "copying $file to /$dirs/$(basename $file)"
+	done
+}
+
+
+# installs base programs like brew to install other dependencies.
+function install_base_progs {
+	echo "install base progs"
+}
+
+function install_progs_brew {
+	echo "install brew progs"
+}
+
+for dir in */
+do
+	if [ $dir == "__homedir/" ];
+	then
+		copy_files true
+		continue
+	fi
+
+	copy_files
+done
+
+install_base_progs
+install_progs_brew
+
