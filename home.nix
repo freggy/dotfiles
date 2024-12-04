f6f6d2c5eb1cd4fd80805e7c0ec92b2fd05bd3f2
yannic rieger <ybr@76k.io>
2024-12-04 22:29:13 +0100

    add git config

--- home.nix
+++ home.nix
@@ -10,4 +10,14 @@ in
     home.file.".zshrc".source = "${files}/.zshrc";
     home.file.".config/zed/settings.json".source = "${files}/zed/settings.json";
     home.file.".config/zed/keymap.json".source = "${files}/zed/keymap.json";
+    programs.git = {
+      enable = true;
+      userName  = "yannic rieger";
+      userEmail = "ybr@76k.io";
+      lfs.enable = true;
+      extraConfig = {
+        commit.gpgsign = true;
+        user.signingkey = "DFEC74EA9AB2B963";
+      };
+    };
   }
