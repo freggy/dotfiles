fa3d9ee58e1ddd499fa9d23d5a6a9b4f079dfe73
yannic rieger <ybr@76k.io>
2024-12-04 18:04:36 +0100

    add zed configs

--- home.nix
+++ home.nix
@@ -8,4 +8,6 @@ in
     home.stateVersion = "24.11";
     home.file.".tmux.conf".source = "${files}/.tmux.conf";
     home.file.".zshrc".source = "${files}/.zshrc";
+    home.file.".config/zed/settings.json".source = "${files}/zed/settings.json";
+    home.file.".config/zed/keymap.json".source = "${files}/zed/keymap.json";
   }
